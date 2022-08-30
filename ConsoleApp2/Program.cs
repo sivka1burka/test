42ad4c05515ed2d67f025e646fd7471c89e036de
sivka1burka <105357398+sivka1burka@users.noreply.github.com>
2022-08-31 01:53:11 +0600

    test
    test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Bank
+    {
+        private int Money;
+        public int money {
+            get 
+            {
+                return Money; 
+            } set 
+            {
+                Money = value;
+                Task save = Task.Run(() => { Save(); });
+                save.Wait();
+            } }
+        private string Name;
+        public string name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+                Task save = Task.Run(() => { Save(); });
+                save.Wait();
+            }
+        }
+        private int Percent;
+        public int percent
+        {
+            get
+            {
+                return Percent;
+            }
+            set
+            {
+                Percent = value;
+                Task save = Task.Run(() => { Save(); });
+                save.Wait();
+            }
+        }
+        public void Save()
+        {
+            
+            string path = @"log.txt";
+            
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                writer.WriteLine(money+" | "+name+" | "+percent);
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            bank.money = 2000;
+            bank.name = "abobus";
+            bank.percent = 1111;
+        }
+    }
+
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo str;
+            Console.WriteLine("Ожидайте!!!");
+            Random rand = new Random();
+            Thread.Sleep(rand.Next(1000, 3000));
+            DateTime start = DateTime.Now;
+            int startMilSec = start.Second * 1000 + start.Millisecond;
+            Console.WriteLine("Нажмите X!!!");
+            str = Console.ReadKey(true);
+            
+            
+            if (str.Key.ToString() == "X")
+            {
+                DateTime finish = DateTime.Now;
+                int finishMilSec = finish.Second * 1000 + finish.Millisecond;
+                int result = finishMilSec - startMilSec;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Нажата неправильная кнопка!");
+            }
+                   
+                    
+           
+            
+            
+        }
+    }
+}
